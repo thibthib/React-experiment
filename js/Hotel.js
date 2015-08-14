f69b9cc2ebe21e97f4ff97e4ea96dc39d83dec71
Thibaut Dutartre <thibaut.dutartre@weekendesk.fr>
2015-08-14 11:42:08 +0200

    Preventing crash when there is no reviews

--- js/Hotel.js
+++ js/Hotel.js
@@ -9,7 +9,7 @@ var Hotel = React.createClass({
       <div className="Hotel">
         <h2 className="Hotel-name">{this.props.data.label}</h2>
         <span className="Hotel-address">{this.props.data.location.address}</span>
-        <span className="Hotel-review">{this.props.data.review.average}</span>
+        <span className="Hotel-review">{this.props.data.review ? this.props.data.review.average : ''}</span>
         <div className="Hotel-weekends">{weekendNodes}</div>
       </div>
     );
