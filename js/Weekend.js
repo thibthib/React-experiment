e563e190e8068618ebf53a043324b1804be2e2c4
Thibaut Dutartre <thibaut.dutartre@weekendesk.fr>
2015-08-14 15:55:16 +0200

    You can now select / unselect a weekend

--- js/Weekend.js
+++ js/Weekend.js
@@ -1,20 +1,21 @@
 var Weekend = React.createClass({
+  getInitialState: function() {
+    return { selected: false };
+  },
+  onClick: function() {
+    this.setState({ selected: !this.state.selected });
+  },
   render: function() {
-    var themesNodes;
-    if (this.props.data.topTheme) {
-      themesNodes = this.props.data.topTheme.map(function (theme) {
-        return (
-          <span className="Weekend-theme">{theme}</span>
-        );
-      });
-    }
+    var createTheme = function(theme, index) {
+      return <span className="Weekend-theme">{theme}</span>;
+    };
   
     return (
-      <div className="Weekend">
+      <div className={ 'Weekend' + (this.state.selected ? ' is-selected' : '') } onClick={this.onClick}>
         <img className="Weekend-image" src={this.props.data.imageUrl}/>
         <div className="Weekend-infos">
           <div className="Weekend-label">{this.props.data.label}</div>
-          <div className="Weekend-themes">{themesNodes}</div>
+          <div className="Weekend-themes">{this.props.data.topTheme && this.props.data.topTheme.map(createTheme)}</div>
         </div>
       </div>
     );
